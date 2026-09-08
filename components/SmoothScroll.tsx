05ed732b1d083e7aa781417b5e50af9254f06eee
BricketX Dev Team <bricketx.dev@gmail.com>
2026-09-08 12:43:54 +0500

    error resolved

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,13 +1,34 @@
 "use client";
 
-import { ReactLenis } from "@studio-freight/react-lenis";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
+import Lenis from "lenis";
 
-export default function SmoothScroll({ children }: { children: ReactNode }) {
-  return (
-    <ReactLenis root options={{ lerp: 0.05, duration: 1.5, smoothWheel: true }}>
-      {/* Cast to any to bypass the React 19 vs 18 type definition mismatch */}
-      {children as any}
-    </ReactLenis>
-  );
+export default function SmoothScroll({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.05,
+      duration: 1.5,
+      smoothWheel: true,
+    });
+
+    let animationFrame: number;
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      animationFrame = requestAnimationFrame(raf);
+    };
+
+    animationFrame = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(animationFrame);
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
 }
